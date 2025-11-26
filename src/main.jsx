79d744b0dc1fecc1498ff851b73b7a0f02697043
JINHYUN-BRIGHTDAY <jinibrighteveryday@gmail.com>
2025-11-26 17:39:27 +0900

    feat : 렌더링

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App01 from './ch01/App01';
 // 컴포넌트
 // const name = "김준일" "김준이";
 
-const currentApp = "ch01";
+// const currentApp = "ch01_1";
+const currentApp = "ch01_2";
 
 const appObj = {
     ch01 : <App01 />,
     // "ch02" : <App02 />,
+    ch01_1: <h1>ch01_1 렌더링</h1>,
+    ch01_2: BoxComponent(),
+    ch01_3: <BoxComponent />,
+    ch01_4: <BoxComponent></BoxComponent>,
 }
 
 // 두 개 이상 태그 -> 예외없이 반드시 감싸야
@@ -16,3 +21,17 @@ const appObj = {
 
 const root = document.getElementById('root');
 createRoot(root).render(appObj[currentApp]);
+
+function BoxComponent() {
+    return <div><TitleComponent /></div>
+}
+
+// function BoxComponent() {
+    // return <div>box</div>
+// }
+// 
+// 
+
+function TitleComponent() {
+    return <h1>타이틀</h1>
+}
